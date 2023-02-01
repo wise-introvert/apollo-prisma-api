572d0552220d328a5aac6ad3f39beb9fc6a06c43
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-02-01 02:07:02 -0500

    feat: setup prisma
    feat: setup prisma
    
    feat: update example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { PrismaClient, Book } from "@prisma/client";
+import * as fs from "fs";
+import * as path from "path";
+
+const prisma = new PrismaClient();
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -9,6 +14,7 @@ const typeDefs = `#graphql
 
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
+    id: String
     title: String
     author: String
   }
@@ -30,15 +36,15 @@ const books = [
     title: "City of Glass",
     author: "Paul Auster",
   },
-]; 
+];
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: async (): Promise<Book[]> => prisma.book.findMany({}),
   },
-}; 
+};
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -47,12 +53,43 @@ const server = new ApolloServer({
   resolvers,
 });
 
+async function main() {
+  console.log("\n\nURL: ", process.env.DATABASE_URL);
+
+  // Connect the client
+  await prisma.$connect();
+  // ... you will write your Prisma Client queries here
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  const promises = books.map((book) =>
+    prisma.book.create({
+      data: book,
+    })
+  );
+
+  const response = await Promise.all(promises);
+
+  fs.writeFileSync(
+    path.join(__dirname, ".", "data.json"),
+    JSON.stringify(response, null, 2)
+  );
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
